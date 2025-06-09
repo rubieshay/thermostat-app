03e226b47474424aa85ac0aadc24ccf9068fe348
David Shay <davideshay@gmail.com>
2025-06-08 22:43:39 -0400

    Attempt android build signed release

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'net.shaytech.gthermostat',
   appName: 'GoogleThermostat',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  },
+  android: {
+    buildOptions: {
+      keystorePath: 'upload-keystore.jks',
+      keystoreAlias: 'upload',
+      // Don't put passwords here - use environment variables
+    }
+  }
 };
 
 export default config;
