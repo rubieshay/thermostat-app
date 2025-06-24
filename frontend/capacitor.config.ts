db03d852ac05d7c9f9dea33b5b507299bd93456d
David Shay <davideshay@gmail.com>
2025-06-24 10:12:53 -0400

    Change android name to ThermoPal

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'net.shaytech.gthermostat',
-  appName: 'GoogleThermostat',
+  appName: 'ThermoPal',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
